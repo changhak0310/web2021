4be14ce3277ba6df8f617743af323c0f0c9fe399
changhak0310 <dlckdgkr0320@naver.com>
2021-06-15 18:19:31 +0900

    reacr 연습

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// interface User {
+//   firstName: string;
+//   lastName: string;
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function formatName(user: User) {
+//   return user.firstName + ' ' + user.lastName;
+// }
+
+// const user: User = {
+//   firstName: "이",
+//   lastName: "창학"
+// }
+
+// const element = <h1 tabIndex={0}>Hello {formatName(user)}</h1>
+
+// function getGreeting(user?: User) {
+//   if(user) {
+//     return <h1>Hello, {formatName(user)}</h1>
+//   }
+//   return <h1>Hello, Stranger.</h1>
+// }
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello world</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   )
+//   ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+//   );
+// }
+
+// setInterval(tick, 1000);
+
+// interface WelmcomeProps {
+//   name: string;
+// }
+
+// 함수 컴포넌트
+// function Welcome(props: WelmcomeProps) {
+//   return <h1>Hello, {props.name}</h1>
+// }
+
+// 클래스 컴포넌트
+// class Welcome extends React.Component<WelmcomeProps> {
+//   render() {
+//     return <h1>Hello, {this.props.name}</h1>
+//   }
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Welcome name="dudco1"/>
+//       <Welcome name="dudco2"/>
+//       <Welcome name="dudco3"/>
+//       <Welcome name="dudco4"/>
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
